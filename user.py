525703726c2af5e75746b268c45b063f1394589d
Benjamin Nudelman <bnudelman40@stuy.edu>
2024-05-28 14:44:11 -0400

    completed client setup

--- user.py
+++ user.py
@@ -22,6 +22,16 @@ def server_setup():
 
     return s
 
+def client_setup(IP):
+    #create a socket
+    s = socket.socket()
+    print ("Socket created")
+
+    s.connect((IP, port))
+    print ("connected to %s on port %s" %(IP) %(port))
+
+    return s
+
 if __name__ == "__main__":
     #user input will be in one the following forms:
         #python3 user.py -SEND FILENAME IP
@@ -31,16 +41,23 @@ if __name__ == "__main__":
     if (sys.argv[1] == "-SEND"):
         file_path = sys.argv[2]
         server_ip = sys.argv[3]
+
+        client_socket = client_setup()
+        
+        #now the client has to recieve information
+
+        # close the connection after all the information is sent
+        client_socket.close()
         
     else if (sys.argv[1] == "-RECIEVE"):
         while !(SENT):
-            socket = server_setup()
+            server_socket = server_setup()
 
             # establish connection when client connects
-            client, address = s.accept()
+            client, address = server_socket.accept()
             print ('established connection from', address )
 
-        # Close the connection
+        # Close the connection when all the required information is sent through the socket
         client.close()
 
     else:
